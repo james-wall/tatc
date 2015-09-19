0fe1ee0023e3cf78f8caa4f82d799c5c227fbae0
James Wall <james.wall@wustl.edu>
2015-09-18 23:28:53 -0500

    php get of api cleaned a lil

--- callAPI.php
+++ callAPI.php
@@ -4,20 +4,26 @@
 
 <?php
 //next example will recieve all messages for specific conversation
-$service_url = 'http://api.tripadvisor.com/api/partner/2.0/location/89575?key=HackTripAdvisor-ade29ff43aed';
+$service_url = 'http://api.tripadvisor.com/api/partner/2.0/location/84943/?key=HackTripAdvisor-ade29ff43aed';
 $curl = curl_init($service_url);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 $curl_response = curl_exec($curl);
+
+
+
 if ($curl_response === false) {
     $info = curl_getinfo($curl);
     curl_close($curl);
-    die('error occured during curl exec. Additioanl info: ' . var_export($info));
+    die('error occured during curl exec. Additional info: ' . var_export($info));
 }
+
+echo($curl_response);
 curl_close($curl);
-$decoded = json_decode($curl_response);
-if (isset($decoded->response->status) && $decoded->response->status == 'ERROR') {
-    die('error occured: ' . $decoded->response->errormessage);
-}
-echo 'response ok!';
-var_export($decoded->response);
+// $decoded = json_decode($curl_response);
+// if (isset($decoded->response->status) && $decoded->response->status == 'ERROR') {
+//     die('error occured: ' . $decoded->response->errormessage);
+// }
+// echo 'response ok!';
+// echo($decoded->response->status);
+//var_export($decoded->response);
 ?>
